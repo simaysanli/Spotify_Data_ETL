210d5edbd67588c9b92c8771749e19388c422570
Simay Sanli <sanli.simay@gmail.com>
2022-03-15 17:15:44 +0300

    implemented load part

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import datetime
 import sqlite3
 
-DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+DATABASE_LOCATION = "sqlite:///my_played_list.sqlite"
 USER_ID = "" #username
 TOKEN = "" #generated token
 
@@ -31,17 +31,19 @@ if __name__ == "__main__":
     #convert to json to dataframe
     playlist_name = []
     owner_list = []
-    total = []
+    playlist_id = []
 
     for playlist in data['items']:
         playlist_name.append(playlist['name'])
         owner_list.append(playlist['owner']['display_name'])
+        playlist_id.append(playlist['id'])
 
     playlist_dict = {
         "playlist_name": playlist_name,
-        "owner_list": owner_list}
+        "owner_list": owner_list,
+        "ID": playlist_id}
 
-    playlist_df = pd.DataFrame(playlist_dict, columns=["playlist_name", "owner_list"])
+    playlist_df = pd.DataFrame(playlist_dict, columns=["ID","playlist_name", "owner_list"])
 
     print(playlist_df)
 
@@ -56,6 +58,41 @@ if __name__ == "__main__":
             raise Exception("Null values found")
 
         return True
+
+    #Validate
+    if check_if_valid_data(playlist_df):
+        print("Data is valid, can be proceeded to Load stage")
+
+
+    #Load
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    connection = sqlite3.connect('my_played_list.sqlite')
+    cursor = connection.cursor()
+
+    sql_query = """
+        CREATE TABLE IF NOT EXISTS my_played_list(
+            playlist_name VARCHAR(200),
+            owner_name VARCHAR(200),
+            ID VARCHAR(200),
+            CONSTRAINT primary_key_constraint PRIMARY KEY (ID)
+        )
+        """
+
+    cursor.execute(sql_query)
+    print("Opened database successfully")
+
+    try:
+        playlist_df.to_sql("my_played_list", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in database")
+
+    connection.close()
+    print("Closed database successfully")
+            
+    
+
+
 
 
 
