0de4f5e94c10b192ce581219e5855187b7ddc568
Simay Sanli <sanli.simay@gmail.com>
2022-03-15 16:13:03 +0300

    transformed spotify data

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ TOKEN = "" #generated token
 
 if __name__ == "__main__":
     headers = {
-        "Accept" : "application/json",
-        "Content-Type" : "application/json",
-        "Authorization" : "Bearer {token}".format(token=TOKEN)
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {token}".format(token=TOKEN)
     }
 
     #retrieve playlists of users
@@ -26,6 +26,36 @@ if __name__ == "__main__":
         headers=headers)
 
     data = r.json()
+    #print(data)
+
+    #convert to json to dataframe
+    playlist_name = []
+    owner_list = []
+    total = []
+
+    for playlist in data['items']:
+        playlist_name.append(playlist['name'])
+        owner_list.append(playlist['owner']['display_name'])
+
+    playlist_dict = {
+        "playlist_name": playlist_name,
+        "owner_list": owner_list}
+
+    playlist_df = pd.DataFrame(playlist_dict, columns=["playlist_name", "owner_list"])
+
+    print(playlist_df)
+
+    def check_if_valid_data(df: pd.DataFrame) -> bool:
+        #Check if dataframe is empty
+        if df.empty:
+            print("No playlists are available in spotify account")
+            return False
+
+        # Check for nulls
+        if df.isnull().values.any():
+            raise Exception("Null values found")
+
+        return True
 
 
 
